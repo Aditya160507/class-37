c1644e5c1adaabf44f06ad493e2a801b39519ecb
Aditya160507 <72134897+Aditya160507@users.noreply.github.com>
2020-12-14 16:55:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var ball;
+var database;
+var gameState=0;
+var allPlayers;
+var playerCount,game,form,player;
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database();
+    game = new Game();
+    game.getState();
+    game.start();
+
+
+}
+
+function draw(){
+  if (playerCount===4){
+    game.update(1);
+  }
+  if(gameState===1){
+    clear ()  
+    game.play()
+  }
+} 
+
